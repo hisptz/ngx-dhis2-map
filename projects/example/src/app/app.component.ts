e9731db633066d7c6e0ea4ec042a7ad72500c3bc
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-04-05 10:30:26 +0300

    feat(application): Added example application to showcase the library

---
+++ projects/example/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hisptz-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'example';
+}
